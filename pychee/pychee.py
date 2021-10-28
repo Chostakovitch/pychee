a129c13a8d93a5c7630b1b5305b210f12a6de134
Quentin Duchemin <quentinduchemin@tuta.io>
2021-10-29 00:36:09 +0200

    Add Photo API

--- pychee/pychee.py
+++ pychee/pychee.py
@@ -13,13 +13,15 @@ from requests import Session
 __version__ = 0.0
 
 class LycheeForbidden(Exception):
-    """Raised when the Lychee request returns 401: Forbidden."""
+    """Raised when the Lychee request is unauthorized."""
+    pass
 
+class LycheeNotFound(Exception):
+    """Raised when the requested ressource was not found."""
     pass
 
 class LycheeError(Exception):
     """Raised for general Lychee errors."""
-
     pass
 
 class LycheeAPISession(Session):
@@ -30,10 +32,14 @@ class LycheeAPISession(Session):
     needs auth and user is not logged in.
     """
 
-    UNAUTH_MESSAGES = [
+    FORBID_MESSAGES = [
         '"Warning: Album private!"',
+        '"Error: validation failed"'
     ]
 
+    NOT_FOUND_MESSAGES = [
+        '"Error: no pictures found!"'
+    ]
     def __init__(self, prefix_url: str, *args, **kwargs):
         """Initialize the `requests.session`."""
         super().__init__(*args, **kwargs)
@@ -43,8 +49,10 @@ class LycheeAPISession(Session):
         """Make an HTTP request with the configured session."""
         url = join(self._prefix_url, 'api', url)
         response = super().request(method, url, *args, **kwargs)
-        if response.text in self.UNAUTH_MESSAGES:
+        if response.text in self.FORBID_MESSAGES:
             raise LycheeForbidden(response.text)
+        if response.text in self.NOT_FOUND_MESSAGES:
+            raise LycheeNotFound(response.text)
         if response.text == 'false' or response.text is None:
             raise LycheeError('Could be unauthorized, wrong args, who knows?')
         return response
@@ -179,9 +187,9 @@ class LycheeClient:
         data = {'albumIDs': dest_id + ',' + ','.join(source_ids)}
         return 'true' in self._session.post('Album::move', data=data).text
 
-    def set_album_licence(self, album_id: str, license: str) -> bool:
+    def set_album_license(self, album_id: str, license: str) -> bool:
         """
-        Set the license of the Album.
+        Set the license of an album.
 
         See https://github.com/LycheeOrg/Lychee/blob/master/app/Assets/Helpers.php
         for authorized licenses (search get_all_licenses())
@@ -196,17 +204,116 @@ class LycheeClient:
         Get a ZIP file of the pictures of the albums and their subalbums.
 
         Archive is returned as bytes, you can open a file
-        with wb mode and write to it.
+        with wb mode and write a file.
         """
         data = {'albumIDs': ','.join(album_ids)}
         # For large archives, maybe we would use
         # stream=True and iterate over chunks of answer.
         return self._session.get('Album::getArchive', params=data).content
 
-    """
-    Get frame mode settings.
-
-    For now, the only setting is the refresh time, in milliseconds.
-    """
     def get_frame_settings(self) -> dict:
+        """
+        Get frame mode settings.
+
+        For now, the only setting returns the refresh time, in milliseconds.
+        """
         return self._session.post('Frame::getSettings').json()
+
+    def get_random_photo(self) -> bytes:
+        """Get a random photo with current auth."""
+        return self._session.post('Photo::getRandom').content
+
+    def set_photos_title(self, photo_ids: List[str], title: str) -> bool:
+        """Set the title of one or multiple photos."""
+        data = {'photoIDs': ','.join(photo_ids), 'title': title}
+        return 'true' in self._session.post('Photo::setTitle', data=data).text
+
+    def set_photo_description(self, photo_id: str, description: str) -> bool:
+        """Set the description of one or multiple photos."""
+        data = {'photoID': photo_id, 'description': description}
+        return 'true' in self._session.post('Photo::setDescription', data=data).text
+
+    def set_photos_star(self, photo_ids: List[str]) -> bool:
+        """
+        Toggle the favorite status of one or multiple photos.
+
+        A starred photo will be unstarred, and vice versa.
+        """
+        data = {'photoIDs': ','.join(photo_ids)}
+        return 'true' in self._session.post('Photo::setStar', data=data).text
+
+    def set_photo_public(self, photo_id: str) -> bool:
+        """
+        Toggle the public status of a photo.
+
+        A public photo will be unstarred, and vice versa.
+        """
+        data = {'photoID': photo_id}
+        return 'true' in self._session.post('Photo::setPublic', data=data).text
+
+    def set_photos_album(self, photo_ids: List[str], album_id: str) -> bool:
+        """Put one or multiple photos into an album"""
+        data = {'photoIDs': ','.join(photo_ids), 'albumID': album_id}
+        return 'true' in self._session.post('Photo::setAlbum', data=data).text
+
+    def set_photos_tags(self, photo_ids: List[str], tags: List[str]) -> bool:
+        """Set tags for one or multiple photos."""
+        data = {'photoIDs': ','.join(photo_ids), 'tags': ','.join(tags)}
+        return 'true' in self._session.post('Photo::setTags', data=data).text
+
+    def add_photo(self, photo: bytes, photo_name: str, album_id: str) -> int:
+        """
+        Upload a photo into an album.
+
+        photo should be open('/your/photo', 'rb').read().
+
+        Return the ID of the uploaded image"""
+        data = {'albumID': album_id}
+        files = {'0': (photo_name, photo)}
+        return int(self._session.post('Photo::add', data=data, files=files).text)
+
+    def delete_photo(self, photo_ids: List[str]) -> bool:
+        """Delete one or multiple photos."""
+        data = {'photoIDs': ','.join(photo_ids)}
+        return 'true' in self._session.post('Photo::delete', data=data).text
+
+    def duplicate_photos(self, photo_ids: List[str], album_id: str) -> bool:
+        """Duplicate one or multiple photos into an album."""
+        data = {'photoIDs': ','.join(photo_ids), 'albumID': album_id}
+        return 'true' in self._session.post('Photo::duplicate', data=data).text
+
+    def set_photo_license(self, photo_id: str, license: str) -> bool:
+        """
+        Set the license of a photo.
+
+        See https://github.com/LycheeOrg/Lychee/blob/master/app/Assets/Helpers.php
+        for authorized licenses (search get_all_licenses())
+
+        Returns false if license name is unrecognized.
+        """
+        data = {'photoID': photo_id, 'license': license}
+        return 'true' in self._session.post('Photo::setLicense', data=data).text
+
+    def get_photos_archive(self, photo_ids: List[str], kind: str) -> bytes:
+        """
+        Get a photo or an archive of photos.
+
+        Kind is one of the following : FULL, LIVEPHOTOVIDEO, MEDIUM2X, MEDIUM, SMALL2X, SMALL, THUMB2X, THUMB.
+
+        If len(photo_ids) == 1, returns an image. Otherwise returns a ZIP archive.
+
+        Content is returned as bytes, you can open a file
+        with wb mode and write a file.
+        """
+        data = {'photoIDs': ','.join(photo_ids), 'kind': kind}
+        return self._session.get('Photo::getArchive', params=data).content
+
+    def clear_photos_symlink(self) -> bool:
+        """
+        Remove all photo's symlinks, if activated.
+
+        Symlinks are disabled by default and allow to create expirable links to photos, preventing to guess the URL of the full sized photo.
+
+        See [the documentation](https://lycheeorg.github.io/docs/settings.html#symbolic-link).
+        """
+        return 'true' in self._session.get('Photo::clearSymLink').text
